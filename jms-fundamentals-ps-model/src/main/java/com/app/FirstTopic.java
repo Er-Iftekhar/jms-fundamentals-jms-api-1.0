2e12b5696eec886b943e23f3972685ba004e5aae
Syed Iftekharuddin <syeydy636@gmail.com>
2025-05-04 09:22:58 +0200

    Add comments to the workflow.

--- jms-fundamentals-ps-model/src/main/java/com/app/FirstTopic.java
+++ jms-fundamentals-ps-model/src/main/java/com/app/FirstTopic.java
@@ -6,31 +6,33 @@ import javax.naming.NamingException;
 
 public class FirstTopic {
     public static void main(String[] args) throws NamingException, JMSException {
+        // Initial context to load the properties from properties file
         InitialContext initialContext = new InitialContext();
+        // Create a topic from initial context
         Topic topic = (Topic) initialContext.lookup("topic/myTopic");
-
+        // Create connectionFatctory to create connections
         ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
-
+        // Create connections to create sessions
         Connection connection = connectionFactory.createConnection();
-
+        // Create sessions to create producer and consumers
         Session session = connection.createSession();
-
+        // Producer to send message
         MessageProducer producer = session.createProducer(topic);
-
+        // Multiple consumers to consume the message
         MessageConsumer consumer1 = session.createConsumer(topic);
         MessageConsumer consumer2 = session.createConsumer(topic);
-
+        // Message sent my the producer
         TextMessage senderMessage = session.createTextMessage("I can do it");
         producer.send(senderMessage);
-
+        // This states that consumer is ready to receive messages
         connection.start();
-
+        // Parsing message received by consumer 1
         TextMessage receivedMessage1 = (TextMessage) consumer1.receive();
         System.out.println("Message received1: " + receivedMessage1.getText());
-
+        // Parsing message received by consumer 2
         TextMessage receivedMessage2 = (TextMessage) consumer2.receive();
         System.out.println("Message received2: " + receivedMessage2.getText());
-
+        // Closing resources
         connection.close();
         initialContext.close();
     }
